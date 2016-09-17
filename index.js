04fff7342a1a3610cecc1427e5b5bcfe7d2b2243
shyam2tayal <shyam2tayal@gmail.com>
2016-09-17 21:22:57 +0530

    App modified

--- index.js
+++
@@ -1,8 +0,0 @@
-
-
-var RepoApp = angular.module('RepoApp', ['ui.router']);
-
-console.log("i was here");
-
-	// create angular controller
-	
--- script/app.js
+++ script/app.js
@@ -4,42 +4,42 @@
 var RepoApp = angular.module('RepoApp', ['ui.router']);
 
 RepoApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-				// For any unmatched url, send to /business
+				
 				$urlRouterProvider.otherwise("/home")
 
 				$stateProvider
 
 						.state('display', {
 							url: "/stats",
-							templateUrl: "display.html"
+							templateUrl: "views/display.html"
 						})
 
 						.state('data', {
 							url: "/org",
-							templateUrl: "data.html"
+							templateUrl: "views/data.html"
 						})
 
 
 			}]);
 
 			RepoApp.controller('mainController', function($scope,$http,$state) {
-											 console.log("i was here");
+											
 
-				// function to submit the form after all validation has occurred
+				
 
 
-     $scope.getdata = function(data){
+    				 $scope.getdata = function(data){
 
 
 				$http.get("https://api.github.com/users/"+data)
 					.then(function(response){
-              console.log(data);
+              
 						$scope.mydata= response.data;
-						console.log("i was here");
+						;
 					});
 
 
-                $state.go('display');
+               			 $state.go('display');
 
 
 				}
@@ -50,7 +50,7 @@ RepoApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
 						.then(function(response){
 
 							$scope.mydata1= response.data;
-							console.log("i was here");
+							
 						});
 
 
